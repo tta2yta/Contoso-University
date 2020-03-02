d50a614dc9c9561fddc0178ab4c65bc185d26ea8
tta2yta <56317574+tta2yta@users.noreply.github.com>
2020-03-02 11:59:27 +0300

    Update Instructor.cs

--- Models/Instructor.cs
+++ Models/Instructor.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ContosoUniversity.Models
+namespace University_MS.Models
 {
     public class Instructor
     {
