e10d106461b978ea507dd8a5fc401ebc547a3637
tta2yta <56317574+tta2yta@users.noreply.github.com>
2020-03-02 11:53:38 +0300

    Update Icontoso_repository.cs

--- Data/Icontoso_repository.cs
+++ Data/Icontoso_repository.cs
@@ -1,10 +1,10 @@
-﻿using ContosoUniversity.Models;
+﻿using University_MS.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContosoUniversity.Data
+namespace University_MS.Data
 {
     public interface Icontoso_repository
     {
