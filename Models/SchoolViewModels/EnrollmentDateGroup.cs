d6f904e37f551f023b9666264d50de0442a9b817
tta2yta <56317574+tta2yta@users.noreply.github.com>
2020-03-02 11:56:46 +0300

    Update EnrollmentDateGroup.cs

--- Models/SchoolViewModels/EnrollmentDateGroup.cs
+++ Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContosoUniversity.Models.SchoolViewModels
+namespace University_MS.Models.SchoolViewModels
 {
     public class EnrollmentDateGroup
     {
