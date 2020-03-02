65c8c5dcd24feeb8b4b5114e71d653b1ab590fca
tta2yta <56317574+tta2yta@users.noreply.github.com>
2020-03-02 11:59:57 +0300

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContosoUniversity.Models
+namespace University_MS.Models
 {
     public class Student
     {
