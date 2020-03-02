7a7c0aa0ca7ef4ac3d1ec68895741dcdd7265c6e
tta2yta <56317574+tta2yta@users.noreply.github.com>
2020-03-02 11:58:59 +0300

    Update Enrollment.cs

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-namespace ContosoUniversity.Models
+namespace University_MS.Models
 {
     public enum Grade
     {
